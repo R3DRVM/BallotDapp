986714c8086370bdec856b11c2b214cf221237dc
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-21 09:02:37 -0800

    Update app.controller.ts

--- Backend/project1/src/app.controller.ts
+++ Backend/project1/src/app.controller.ts
@@ -1,30 +1,58 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { ethers } from 'ethers';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Post } from '@nestjs/common/decorators';
+import { Body } from '@nestjs/common/decorators';
+import { Query } from '@nestjs/common/decorators';
 import { Address } from 'cluster';
-import { AppService, mintedTokens, voteHistory } from './app.service';
-
-export class mintingDto {
-  address: string;
-  amount: string;
-}
+import { ethers } from 'ethers';
+import { stringify } from 'querystring';
+import { AppService, castVoteDto, delegateVotes, mintTokens } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('token-address')
-  getTokenAddress(){
-    return { result: this.appService.getTokenAddress()};
+  getTokenAddress() {
+    return { result: this.appService.getTokenAddress() };
   }
 
   @Post('request-tokens')
-  async requestTokens(@Body() body: mintingDto): Promise<any> {
-    return await this.appService.requestTokens(body);
-  }  
-  
-  @Post('delegate-votes')
-  delegateVotes(@Body() body) {
-    return { result: this.appService.delegateVotes(body)};
+  async requestTokens(@Body() body: mintTokens): Promise<string> {
+    return await this.appService.requestTokens(body.mintToAddress, body.tokenAmnt);
   }
- 
+
+  @Post('self-delegate')
+  async selfDelegate(@Body() body: delegateVotes): Promise<string> {
+    return await this.appService.selfDelegate(body.delegatee);
+  }
+
+  @Get('voting-power/:address')
+  checkVotingPower(@Param ('address') address: string): Promise<number> {
+    return this.appService.checkVotingPower(address)
+  }
+
+  @Get('token-balance/:address')
+  tokenBalance(@Param ('address') address: string): Promise<number> {
+    return this.appService.tokenBalance(address)
+  }
+
+  @Get('ballot-address')
+  getBallotAddress() {
+    return { result: this.appService.getBallotAddress() };
+  }
+
+  @Get('proposals')
+  getProposals() {
+    return this.appService.getProposals();
+  }
+
+  @Get('winning-proposal')
+  async getWinner() {
+    return  await  this.appService.getWinner() ;
+  }
+
+  @Post('cast-vote')
+  async castVote(@Body() body: castVoteDto): Promise<string> {
+    return await this.appService.castVote(body.voterAddress, body.proposal, body.tokenAmount);
+  }
 }
