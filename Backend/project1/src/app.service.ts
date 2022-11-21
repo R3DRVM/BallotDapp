380a6a7bbd95e2f39f8dd836470e5be1b16b9e9d
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-21 09:02:14 -0800

    Update app.service.ts

--- Backend/project1/src/app.service.ts
+++ Backend/project1/src/app.service.ts
@@ -1,85 +1,174 @@
 import { Injectable } from '@nestjs/common';
 import { Address } from 'cluster';
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import * as tokenJson from './assets/MyToken.json';
 import * as ballotJson from './assets/TokenizedBallot.json';
 import * as dotenv from "dotenv";
-import { mintingDto } from './app.controller';
 
-export class mintedTokens {
-  mintToAddress: Address;
-  tokenAmount: number;
+
+export class mintTokens {
+  mintToAddress: string;
+  tokenAmnt: string;
 }
 
-export class tokensDelegated {
-  receiver: Address;
+export class delegateVotes {
+  delegatee: Address;
 }
 
-export class voteHistory {
-  proposalId: string;
-  voteAmount: string; 
+export class castVoteDto {
+  voterAddress: string;
+  proposal: string;
+  tokenAmount: string;
 }
 
-const tokenAddress = "0x350e655770e02e05B4f22169A7b8d3d5aAd6B46a";
-const ballotAddress = "0xf185087be41f7ae83690998f97c8c512b3a55f00";
-const MINT_VALUE = ethers.utils.parseEther("10");
+const erc20VotesAddress = "0x350e655770e02e05B4f22169A7b8d3d5aAd6B46a";
+const ballotAddress = "0x7bFF174e20dBe67936cE74Ea56595b2D57aeB472";
+
+dotenv.config();
 
 
 @Injectable()
 export class AppService {
 
   provider: ethers.providers.AlchemyProvider;
-  erc20ContractFactory: ethers.ContractFactory;
-  erc20Contract: ethers.Contract;
+  tokenContractFactory: ethers.ContractFactory;
+  tokenContract: ethers.Contract;
+
   ballotContractFactory: ethers.ContractFactory;
   ballotContract: ethers.Contract;
-  proposals: string[] | undefined;
-
 
   constructor() {
-    //provider
-    this.provider =  new ethers.providers.AlchemyProvider('goerli', process.env.ALCHEMY_API_KEY ?? "");
-    const erc20ContractFactory = new ethers.ContractFactory(
+    // provider
+    this.provider = new ethers.providers.AlchemyProvider('goerli', process.env.ALCHEMY_API_KEY ?? "");
+      
+    // Attach to MyToken smart contract facotry with the signer
+    this.tokenContractFactory = new ethers.ContractFactory(
       tokenJson.abi,
       tokenJson.bytecode,
     );
-    const Wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-    const signer = Wallet.connect(this.provider);
-    this.erc20Contract = erc20ContractFactory.attach(tokenAddress).connect(this.provider);
-
-    this.ballotContractFactory= new ethers.ContractFactory(ballotJson.abi, ballotJson.bytecode);
-    this.ballotContract = this.ballotContractFactory.attach(ballotAddress).connect(this.provider);
-
-  }
-
-  getProposals() {
-    return this.proposals;
-  }
- 
+    
+    this.tokenContract = this.tokenContractFactory
+    .attach(erc20VotesAddress)
+    .connect(this.provider);
+    
+  // Attach to Ballot smart contract facotry with the signer
+    this.ballotContractFactory = new ethers.ContractFactory(
+      ballotJson.abi,
+      ballotJson.bytecode,
+    );
+    
+    this.ballotContract = this.ballotContractFactory
+    .attach(ballotAddress)
+    .connect(this.provider);
+    }  
+  
   getTokenAddress() {
-    return tokenAddress;
+    return erc20VotesAddress;
   }
 
-  async requestTokens(body: mintingDto) {
-    //minting
-    const minterWallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
-    const minter = minterWallet.connect(this.provider);
-    const mintingStart = this.erc20ContractFactory.attach(tokenAddress).connect(minter);
-    console.log(`Minting tokens to ${minter.address}`);
-    const mintAmount = ethers.utils.parseEther(body.amount);
-    const mintTx = await this.erc20Contract.mint(body.address, mintAmount);
-    await mintTx.wait();
-    console.log(`Successfully minted tokens, tx hash is: ${mintTx.hash}`);
-    const txHash: string = mintTx.hash;
-    return txHash;
-    //await tx
-    //return tx hash
+  getBallotAddress() {
+    return ballotAddress;
   }
-  async delegateVotes(body: any) {
-    console.log('Delegating votes...');
-    const delegate = await this.erc20Contract.delegate(body.address);
-    await delegate.wait();
-    console.log(`Delegation successful, Tx Hash: ${delegate.hash}`);
-    return delegate.hash;
+
+  async getProposals() {
+    const props = await this.ballotContract.proposals();
+    console.log(`props: ${props}\n`)
+    props.forEach((element:string, index:number) => {
+      console.log(`Proposal ${index}: ${element}`)
+    });
+    return props;
   }
- }
+
+  async getWinner() {
+    const winningProposal = await this.ballotContract.winningProposal();
+    const winnerName = ethers.utils.parseBytes32String(await this.ballotContract.winnerName());
+    console.log(`Winning proposal: ${winningProposal}, Winner name: ${winnerName}\n`);
+    return winnerName.toLowerCase();
+  }
+
+  async castVote(voterAddress: string, votedOnProposal:string, tokenAmount:string) {
+    
+    // Connect Wallet to the provider as voter
+    console.log("connecting to a wallet...\n");
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? "");
+    const signer = wallet.connect(this.provider);
+    const contractInstance = this.ballotContractFactory
+      .attach(ballotAddress)
+      .connect(signer);
+
+        // Mint some tokens
+      const oneEther = BigNumber.from("1000000000000000000");
+      const tokenValue = (oneEther).mul(tokenAmount);
+
+      const voteTx = await contractInstance.vote(votedOnProposal, tokenValue);
+      await voteTx.wait();
+      console.log(`voted on proposal ${votedOnProposal} with ${tokenAmount} tokens\n`)
+      console.log(`Transaction hash: ${voteTx.hash}\n`)
+      return voteTx.hash;
+  }
+
+
+  async requestTokens(toAddress: string, amount: string) {
+    
+    // Connect Wallet to the provider as minter
+    console.log("connecting to a wallet...\n");
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
+    const signer = wallet.connect(this.provider);
+
+    // Signer to send the transaction
+    const contractInstance = this.tokenContractFactory
+      .attach(erc20VotesAddress)
+      .connect(signer);
+    
+    // Mint some tokens that matter - let's have a party!!!
+    const oneEther = BigNumber.from("1000000000000000000");
+    // const mintValue = (ethers.utils.parseEther(amount)).mul(oneEther);
+    const mintValue = (oneEther).mul(BigNumber.from(1));
+    
+    // Mint some tokens
+    const mintTx = await contractInstance.mint(toAddress, mintValue);
+    // const mintTx = await this.tokenContract.mint(toAddress, amount);
+    await mintTx.wait()
+    console.log(`Minted ${mintValue} tokens for ${amount} Ether to account ${toAddress}\n`);
+    console.log(`Transaction hash is ${mintTx.hash}\n`)
+    const TxHash: string = mintTx.hash;
+    return TxHash;
+  }
+
+  async selfDelegate(delegateeAddr: Address) {
+     
+    // Connect Wallet to the provider as delegatee
+     console.log("connecting to a wallet...\n");
+     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_5 ?? "");
+     const signer = wallet.connect(this.provider);
+ 
+     // Signer to send the transaction
+     const contractInstance = this.tokenContractFactory
+       .attach(erc20VotesAddress)
+       .connect(signer);
+    
+    //const delegateTx = await contractInstance.delegate(delegateeAddr);
+    const delegateTx = await contractInstance.delegate(signer.address);
+    await delegateTx.wait();
+    const TxHash: string = delegateTx.hash;
+    return TxHash;
+  }
+
+  async checkVotingPower(accountAddr: string): Promise<number> {
+    console.log(`Checking votes for address: ${accountAddr}\n`);
+    // const contractInstance = this.tokenContractFactory.attach(ER20VOTES_TOKEN_ADDRESS).connect(this.provider);
+    const votes = await this.tokenContract.getVotes(accountAddr);
+    console.log(`Account ${accountAddr} has a balance of ${votes} voting power\n`);
+    const vPwr = await this.ballotContract.votingPower(accountAddr);
+    console.log(`Account ${accountAddr} has a balance of ${vPwr} voting power\n`);
+    return parseFloat(ethers.utils.formatEther(vPwr));
+  }
+
+  async tokenBalance(accountAddr: string): Promise<number> {
+    console.log(`Checking token balance for address: ${accountAddr}\n`);
+    // const contractInstance = this.tokenContractFactory.attach(ER20VOTES_TOKEN_ADDRESS).connect(this.provider);
+    const tokenBalance = await this.tokenContract.balanceOf(accountAddr);
+    console.log(`Account ${accountAddr} has a balance of ${tokenBalance} tokens\n`)
+    return parseFloat(ethers.utils.formatEther(tokenBalance));
+  }
+}
